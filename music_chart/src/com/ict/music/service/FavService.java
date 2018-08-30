d886b8463e3fea0e80a0e2f49dd7b116bb239ef4
oxy <oxy@220.116.174.30>
2018-08-31 00:13:18 +0900

    asd

--- music_chart/src/com/ict/music/service/FavService.java
+++ music_chart/src/com/ict/music/service/FavService.java
@@ -1,12 +1,15 @@
 package com.ict.music.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.ict.music.vo.FavInfo;
 
 public interface FavService {
 	public List<FavInfo> getFiList(FavInfo fi);
 	public FavInfo getFi(Long mcNum);
-	public Map<String,Object> init
+	public Map<String,Object> addFavInfo(FavInfo fi);
+	public Map<String,Object> modifyFavInfo(FavInfo fi);
+	public Map<String,Object> removeFavInfo(FavInfo fi);
 	
 }
--- music_chart/src/com/ict/music/service/impl/FavServiceImpl.java
+++ music_chart/src/com/ict/music/service/impl/FavServiceImpl.java
@@ -1,5 +1,41 @@
 package com.ict.music.service.impl;
 
-public class FavServiceImpl {
+import java.util.List;
+import java.util.Map;
+
+import com.ict.music.service.FavService;
+import com.ict.music.vo.FavInfo;
+
+public class FavServiceImpl implements FavService {
+
+	@Override
+	public List<FavInfo> getFiList(FavInfo fi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FavInfo getFi(Long mcNum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> addFavInfo(FavInfo fi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> modifyFavInfo(FavInfo fi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> removeFavInfo(FavInfo fi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
